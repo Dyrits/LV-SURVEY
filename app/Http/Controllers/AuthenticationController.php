95a62e39bf6b4cd4003684c8b63150a12fd201ec
Dyrits <dyrtis@outlook.com>
2023-02-19 13:45:39 +0100

    Add: AuthenticationController.php

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
+class AuthenticationController extends Controller
+{
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => ['required', 'confirmed', Password::min(8)->letters()->numbers()->symbols()->uncompromised()]
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+        $token = $user->createToken('authentication')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+}
